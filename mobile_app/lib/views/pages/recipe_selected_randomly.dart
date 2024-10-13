899c65b771fe2bd5867765ba113de1082d8f8d36
tiktaksick <shankousongshi@gmail.com>
2024-10-13 18:45:23 +0900

    Delete unused recipe_selected_randomly.dart file

--- mobile_app/lib/views/pages/recipe_selected_randomly.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:gap/gap.dart';
-
-import 'my_page.dart';
-import '../utils/colors.dart';
-import "../utils/button_styles.dart";
-import "../utils/text_styles.dart";
